164e2064802c10cde5cd484beba1f3e6d9b6d777
IT22927002 <127732835+IT22927002@users.noreply.github.com>
2023-03-19 22:36:43 +0530

    Update Tute02.c

--- Tute02.c
+++ Tute02.c
@@ -16,9 +16,26 @@ Amount = 20 x 50 = 1000
 Distance -> 50
 Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
-#include <stdio.h>
-
 int main() {
+  //declare variables
+	int distance;
+	int Amount;
+  
+	//input distance
+	printf("Enter the distance the van has travelled:");
+	scanf("%d",&distance);
+  
+	//check wheather the distance is higher than 30 or not
+		if(distance >= 30){
+	
+			Amount = 30*50+(distance-30)*40;
+			}
+			
+		else
+			Amount = distance *50;
+  
+	//output amount		
+  	printf("Amount is %d",Amount);
   
   return 0;
-}
+}//end program
